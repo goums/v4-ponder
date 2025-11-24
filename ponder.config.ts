5e45dccb3d82bbd2cc9301cdf3d8aa92ce5abcbd
Sylvain Goumy <goumsss@protonmail.com>
2025-11-24 10:58:30 +0100

    chore: remove end blocks

--- ponder.config.ts
+++ ponder.config.ts
@@ -46,44 +46,44 @@ export default createConfig({
 				mainnet: {
 					address: "0x000000000004444c5dc75cb358380d2e3de08a90",
 					startBlock: 21_700_000,
-					endBlock: 22_500_000,
+					// endBlock: 22_500_000,
 				},
 				unichain: {
 					address: "0x1f98400000000000000000000000000000000004",
 					startBlock: 0,
-					endBlock: 15_500_000,
+					// endBlock: 15_500_000,
 				},
 				arbitrum: {
 					address: "0x360e68faccca8ca495c1b759fd9eee466db9fb32",
 					startBlock: 297_842_872,
-					endBlock: 320_000_000,
+					// endBlock: 320_000_000,
 				},
 				optimism: {
 					address: "0x9a13f98cb987694c9f086b1f5eb990eea8264ec3",
 					startBlock: 131_000_000,
-					endBlock: 136_000_000,
+					// endBlock: 136_000_000,
 				},
 				base: {
 					address: "0x498581ff718922c3f8e6a244956af099b2652b2b",
 					startBlock: 25_500_000,
-					endBlock: 28_500_000,
+					// endBlock: 28_500_000,
 				},
 
 				// Testnets
 				ethereum_sepolia: {
 					address: "0xE03A1074c86CFeDd5C142C4F04F1a1536e203543",
 					startBlock: 7_258_946,
-					endBlock: 9_500_000,
+					// endBlock: 9_500_000,
 				},
 				base_sepolia: {
 					address: "0x05E73354cFDd6745C338b50BcFDfA3Aa6fA03408",
 					startBlock: 19_088_197,
-					endBlock: 20_500_000,
+					// endBlock: 20_500_000,
 				},
 				arbitrum_sepolia: {
 					address: "0xFB3e0C6F74eB1a21CC1Da29aeC80D2Dfe6C9a317",
 					startBlock: 10_590_922,
-					endBlock: 15_500_000,
+					// endBlock: 15_500_000,
 				},
 			},
 			abi: PoolManagerAbi,
